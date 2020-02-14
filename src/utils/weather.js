201c799ca7fb6084c4728096f8702d98b5e307f1
GUNJAN TEOTIA <aakashverma602@gmail.com>
2020-02-15 01:00:18 +0530

    Added some more data to weather details

--- src/utils/weather.js
+++ src/utils/weather.js
@@ -1,7 +1,7 @@
 const request = require('request');
 
 const weather = (latitude, longitude, callback) => {
-    const url = 'https://api.darksky.net/forecast/40d6dc4d2fce297310a0705056f0398a/' + latitude + ',' + longitude;
+    const url = 'https://api.darksky.net/forecast/40d6dc4d2fce297310a0705056f0398a/' + latitude + ',' + longitude + '?units=si';
 
     request({ url, json: true}, (error, { body }) => {
         if (error) {
@@ -9,7 +9,8 @@ const weather = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined);
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.apparentTemperature + ' degrees and there is a ' + body.currently.precipProbability + ' % chance of precipitation');
+            console.log(body)
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.apparentTemperature + ' degrees out. Today it will be highest as ' + body.daily.data[0].temperatureHigh + ' degree and lowest as ' + body.daily.data[0].temperatureLow + ' degree .There is a ' + body.currently.precipProbability + ' % chance of precipitation');
         }
     })
 };
